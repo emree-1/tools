d5d3dc31a55dc096d287c34887b654694f47bff8
emre <cil.emrehan@yahoo.com>
2025-02-01 17:27:17 +0100

    Updated b64
    Removed some arguments, improved performances and fixed some bugs

--- base64/b64.py
+++ base64/b64.py
@@ -4,7 +4,8 @@ import re
 import base64
 import pandas as pd
 import argparse
-import tabulate
+from tabulate import tabulate
+import warnings
 
 def parse_arguments() :
     parser = argparse.ArgumentParser(description="Quick script to search for keywords in a text file.")
@@ -14,11 +15,8 @@ def parse_arguments() :
     parser.add_argument("--decode", action='store_true', default=False, help="If defined, outputs will tried to be decoded .")
     parser.add_argument("--ctf", action='store_true', default=False, help="If defined, will return values only ending with \"}\".")
     parser.add_argument("-r", "--render", type=str, choices=["csv", "json", "txt", "tab"], default="csv", help="Specify the format to render the output. Available formats: %(choices)s. Default format is %(default)s.")
-    parser.add_argument("--outfile", type=str, default=None, help="output filte to save in.")
     parser.add_argument("-e", "--extract",  type=str, default=None, help="extract result in a seperated file")
-    parser.add_argument("--tblfmt",         type=str, choices="github", default="github", help="Format de la table.")
     parser.add_argument("--index",       action='store_true', default=False, help="If defined, add index to output.")
-    parser.add_argument("--quiet",      action='store_true', help="Don't print output.")
     parser.add_argument("--no_space",       action='store_true', help="Remove spaces at the beginning and at the end of the print.")
     args = parser.parse_args()
     return args
@@ -31,76 +29,65 @@ def write_file(filename, output) :
     with open(filename, 'w') as file :
         file.write(output)
 
-def render(a, filename, format, no_space, headers, tblfmt, index, quiet, header=False):
+def render(a, filename, format, no_space, index, header=False):
     formatters = {
-        "txt": lambda: a.to_string(filename, index= index, header=header),
+        "txt": lambda: a.to_csv(filename, index=False, header=False),
         "csv": lambda: a.to_csv(filename, index=index, header=header),
         "json": lambda: a.to_json(filename, index=index),
-        "tab": lambda: tabulate(a, headers=headers, tablefmt=tblfmt, showindex=False)
+        "tab": lambda: tabulate(a, headers=["Result"], tablefmt="github", showindex=False)
     }
     
     output = "{}{} {}".format('\n' if not no_space else '', formatters.get(format, lambda: "Invalid format")(), '\n' if not no_space else '')
     if filename and format == "tab":
         write_file(filename, output)
-    if not filename and not quiet:
+    if not filename :
         print(output)
 
-def ctf_flags(results) :
-    flags = []
-    for result in results : 
-        if result[-1] == "}" :
-            flags.append(result)
-    return flags    
+def ctf_flags(results):
+    return [result for result in results if result.endswith("}")]
 
 def b64_encode(keyword, precision=-2):
     b64 = base64.b64encode(keyword.encode('utf-8')).decode('utf-8')
     return b64.rstrip("=")[:precision]
 
 def b64_decode(encoded_strings):
-    decoded = []
-    for encoded_string in encoded_strings :
+    for encoded_string in encoded_strings:
         try:
-            padding = len(encoded_string) % 4
-            if padding != 0:
-                encoded_string += "=" * (4 - padding)
-
+            encoded_string = encoded_string.ljust(len(encoded_string) + (4 - len(encoded_string) % 4) % 4, "=")
             decoded_bytes = base64.b64decode(encoded_string, validate=True)
-            
-            decoded_string = decoded_bytes.decode('utf-8', errors='ignore')
-            decoded.append(decoded_string)
-        except Exception as e:
-            print(f"Error : {encoded_string} - {str(e)}")
-            return None
-    return decoded
+            yield decoded_bytes.decode('utf-8', errors='ignore')
+        except (base64.binascii.Error, ValueError) as e:
+            warnings.warn(f"Error decoding: {encoded_string} - {e}")
 
 def find_base64_in_file(file_path, prefix):
-    base64_pattern = re.compile(r'[A-Za-z0-9+/=]{4,}')
-    results = []
-    with open(file_path, 'r') as file:
+    base64_pattern = re.compile(r'\b[A-Za-z0-9+/=]{4,}')
+
+    with open(file_path, 'r', encoding='utf-8') as file:
         for line in file:
             for match in base64_pattern.finditer(line):
-                base64_str = match.group(0)
+                base64_str = match[0]
                 
                 if base64_str.startswith(prefix):
                     try:
                         base64.b64decode(base64_str, validate=True)
-                        results.append(base64_str)
+                        yield base64_str 
                     except (base64.binascii.Error, ValueError):
-                        continue
-    return results
+                        pass
 
 def main() :
     args = parse_arguments()
     prefix = b64_encode(args.prefix) if args.encode else args.prefix
     results = set(find_base64_in_file(args.file, prefix))
-
+    
     if args.decode :
-        results = b64_decode(results)
-    if args.ctf : 
+        results = list(b64_decode(results))
+    if args.ctf :
         results = ctf_flags(results)
-        
-    df = pd.DataFrame(results, columns=["result"])
-    render(df, args.extract, args.render, args.no_space, None, args.tblfmt, args.index, args.quiet)
+    
+    df = pd.DataFrame(results)
+    pd.set_option('display.max_colwidth', None)
+
+    render(df, args.extract, args.render, args.no_space, args.index)
     
 if __name__ == "__main__" :
     main()
